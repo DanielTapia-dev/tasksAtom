03ec536278ee5082ac8d98ed806351751575a5c6
DanielTapia-dev <d.tapialopez1@gmail.com>
2025-01-14 03:05:10 -0500

    feat: implement cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import taskRoutes from './app/routes/task.route';
 import userRoutes from './app/routes/user.route';
 import {errorHandler} from './middlewares/error-handler';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware para parsear JSON
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/task', taskRoutes);
 app.use('/user', userRoutes);
